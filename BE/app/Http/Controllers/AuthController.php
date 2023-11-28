6450046548d2ae054091a43c52939b0c416a8eb1
Alireza Hatami <itshatami@gmail.com>
2023-11-28 14:57:18 +0330

    authentication auth method

--- BE/app/Http/Controllers/AuthController.php
+++ BE/app/Http/Controllers/AuthController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Library\SendSMS;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function auth(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'mobile' => 'required|min:10'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status' => false, 'message' => $validator->messages()]);
+        }
+        $user = User::where('mobile', $request->moblie)->first();
+        if(!$user){
+            return response()->json(['status'=>false , 'message'=>'user does not exist']);
+        }
+    }
 }

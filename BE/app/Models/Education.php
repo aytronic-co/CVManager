6660e6e0049bb690e733b338e872f9255907a05c
halimeKhojamli <h.khojamli@gmail.com>
2023-12-07 11:38:55 +0330

    education controller-> done

--- BE/app/Models/Education.php
+++ BE/app/Models/Education.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Education extends Model
 {
     use HasFactory;
-    protected $quarded = [];
+    protected $guarded = [];
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
